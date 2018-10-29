38b21517f738571cdd403a86b5695994526e2835
a fool <afoolsbag@gmail.com>
2018-10-29 13:51:08 +0800

    feat(rrnode): types.test.js

--- rrnode/types.test.js
+++ rrnode/types.test.js
@@ -5,25 +5,43 @@ test('Undefined', () => {
 })
 
 test('Null', () => {
-  expect(typeof null).toBe('object')
+  const n = null
+  expect(typeof n).toBe('object')
 })
 
 test('Number', () => {
-  expect(typeof Object()).toBe('object')
+  const n = 1337
+  expect(typeof n).toBe('number')
 })
 
 test('String', () => {
-
+  const s = 'hello, world'
+  expect(typeof s).toBe('string')
 })
 
 test('Boolean', () => {
-
+  const b = false
+  expect(typeof b).toBe('boolean')
 })
 
 test('Symbol', () => {
-
+  const s = Symbol()
+  expect(typeof s).toBe('symbol')
 })
 
 test('Object', () => {
+  const o = {}
+  expect(typeof o).toBe('object')
 
+  const f = function () {}
+  expect(typeof f).toBe('function')
+
+  const a = []
+  expect(Array.isArray(a)).toBe(true)
+
+  const d = new Date()
+  expect(typeof d).toBe('object')
+
+  const r = /regex/
+  expect(typeof r).toBe('object')
 })
