70716ea206b641600382f6a9306393fbf6500438
zhengrr <afoolsbag@gmail.com>
2020-07-19 06:38:16 +0800

    chore: rename package to lower (1/2)

--- rrhtml(rename2lower)/run-http-server-via-npx.cmd
+++ rrhtml(rename2lower)/run-http-server-via-npx.cmd
@@ -1,19 +1,20 @@
 @ECHO OFF
 CHCP 65001
 SETLOCAL ENABLEEXTENSIONS
+SET script_directory=%~dp0
 
-WHERE /Q npm ^
-        || ECHO The npm executable not found. ^
+WHERE /Q npx ^
+        || ECHO The npx executable not found. ^
         && CALL :pause_if_double_click ^
         && EXIT /B 1
 
-npm install --global http-server ^
-        || ECHO npm install http-server failed. ^
+CD "%script_directory%" ^
+        || ECHO Change directory to project directory failed. ^
         && CALL :pause_if_double_click ^
         && EXIT /B 2
 
-npm update --global http-server ^
-        || ECHO npm update http-server failed. ^
+npx http-server -o ^
+        || ECHO Run http-server via npx failed. ^
         && CALL :pause_if_double_click ^
         && EXIT /B 3
 
